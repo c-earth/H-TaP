b0b2e36450d32ee7542bcdd14b7be24a64ac0725
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-08-27 17:10:19 -0400

    plot computational

--- 02_Computational/CDF/TaP_cdf.py
+++ 02_Computational/CDF/TaP_cdf.py
@@ -45,36 +45,41 @@ def plot_cdf(E_dos, DoS, CDF, V, suffix, zoom_range = None):
         scale = 1000
         Eunit = 'meV'
 
-    fig, (ax0, ax1) = plt.subplots(2, 1, sharex = True, figsize = (14, 14), dpi = 300)
-    fig.subplots_adjust(hspace=0)
+    fig, (ax0, ax1) = plt.subplots(2, 1, sharex = True, figsize = (8, 7), height_ratios=[1, 3])
+
     ax2 = ax1.twinx()
     def convert_ax2(ax1):
         y1, y2 = ax1.get_ylim()
         ax2.set_ylim(y1/V*1E24, y2/V*1E24)
         ax2.figure.canvas.draw()    
-
     ax1.callbacks.connect("ylim_changed", convert_ax2)
-
-    ax0.plot(E_dos*scale, DoS, 'r-', linewidth = 2)
-    ax0.axvline(x = 0, linewidth = 1, color = 'k', linestyle = (0, (8, 10)))
-    ax0.fill_between(E_dos*scale, 0, DoS, where = (E_dos <= 0), facecolor = 'red', alpha = 0.25)
-
-    ax1.plot(E_dos*scale, CDF - CDF_f, 'r-', linewidth = 2)
-    ax1.axvline(x = 0, linewidth = 1, color = 'k', linestyle = (0, (8, 10)))
-    ax1.axhline(y = 0, linewidth = 1, color = 'k', linestyle = (0, (8, 10)))
+    
+    ax0.axvline(x = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+    ax0.plot(E_dos*scale, DoS, '-', color = '#E33119', linewidth = 3)
+    ax0.fill_between(E_dos*scale, 0, DoS, where = (E_dos <= 0), facecolor = '#E33119', alpha = 0.25)
 
     ax0.set_xlim(min(E_dos)*scale, max(E_dos)*scale)
-    ax0.set_ylabel('DOS []', fontsize = 24)
-    ax0.tick_params(axis = 'both', which = 'both', width = 2, length = 10, direction = 'in', labelsize = 24)
+    ax0.set_ylabel('DOS []', fontsize = 20)
+    ax0.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
 
-    ax1.set_xlabel(r'$E-E_f$ ' + f'[{Eunit}]', fontsize = 24)
-    ax1.set_ylabel(r'CDF - CDF$_f$ []', fontsize = 24)
-    ax1.tick_params(axis = 'both', which = 'both', width = 2, length = 10, direction = 'in', labelsize = 24)
+    ax1.axvline(x = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+    ax1.axhline(y = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+    ax1.plot(E_dos*scale, (CDF - CDF_f)*scale, '-', color = '#E33119', linewidth = 3)
 
-    ax2.set_ylabel(r'Doped Electron [cm$^{-3}$]', fontsize = 24)
-    ax2.tick_params(axis = 'both', which = 'both', width = 2, length = 10, direction = 'in', labelsize = 24)
-    ax2.yaxis.get_offset_text().set_size(24)
-    plt.savefig(f'TaP_dos{suffix}.png')
+    ax1.set_xlabel(r'$E-E_f$ ' + f'[{Eunit}]', fontsize = 20)
+    ax1.set_ylabel(r'CDF - CDF$_f$ [$\times 10^{-3}$]', fontsize = 20)
+    ax1.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+    ax2.set_ylabel(r'Doped Electron [cm$^{-3}$]', fontsize = 20)
+    ax2.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+    ax2.ticklabel_format(axis = 'y', style = 'sci', scilimits = (0, 0), useMathText = True)
+    ax2.yaxis.get_offset_text().set_size(20)
+
+    fig.subplots_adjust(left = 0.15)
+    fig.subplots_adjust(right = 0.875)
+    fig.subplots_adjust(top = 0.95)
+    fig.subplots_adjust(hspace = 0)
+
+    plt.savefig(f'TaP_dos{suffix}.png', dpi = 300)
 
 plot_cdf(E_dos, DoS, CDF, V, suffix)
 plot_cdf(E_dos, DoS, CDF, V, suffix+'_zoom', zoom_range = zoom_range)
@@ -111,14 +116,20 @@ k = np.cumsum(k_point_diff)
 N = math.floor((len(ks)-1)/(len(labels)-1))
 tick_pos = [k[idx] for idx in range(0, len(k), N)]
 
-plt.figure(dpi = 300)
-for b in bs:
-    plt.plot(k, b, linewidth = 1, alpha = 1, color = 'k')
-plt.xlim((np.min(k), np.max(k)))
+fig, ax = plt.subplots(1, 1, figsize = (8, 7))
 for pos in tick_pos:
-    plt.axvline(pos, linewidth = 1, color='k', alpha=0.5)
-plt.axhline(0, linewidth = 1, color = 'k', alpha=1, linestyle = (0, (8, 10)))
-plt.xticks(ticks = tick_pos, labels = labels)
-plt.ylabel(r'$E-E_f$ [eV]')
-plt.ylim((min(E_dos), max(E_dos)))
-plt.savefig(f'TaP_band{suffix}.png')
+    ax.axvline(pos, linewidth = 2, color='k', alpha = 1)
+ax.axhline(0, linewidth = 2, color = 'k', alpha = 1, linestyle = (0, (8, 10)))
+for b in bs:
+    ax.plot(k, b, '-', linewidth = 2, alpha = 1, color = '#E33119')
+ax.set_xlim((np.min(k), np.max(k)))
+ax.set_xticks(ticks = tick_pos, labels = labels, fontsize = 20)
+ax.set_ylabel(r'$E-E_f$ [eV]', fontsize = 20)
+ax.set_ylim((min(E_dos), max(E_dos)))
+ax.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+ax.ticklabel_format(axis = 'y', style = 'sci', scilimits = (0, 0), useMathText = True)
+ax.yaxis.get_offset_text().set_size(20)
+
+fig.tight_layout()
+
+plt.savefig(f'TaP_band{suffix}.png', dpi = 300)
---
+++ 02_Computational/SuperCell/Weyls_points.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from matplotlib.collections import LineCollection
+from matplotlib.patches import Rectangle
+
+pris_file = 'D:/python_project/H-TaP/02_Computational/SuperCell/pristine.csv'
+dope_file = 'D:/python_project/H-TaP/02_Computational/SuperCell/withH.csv'
+
+pris_data = np.genfromtxt(pris_file, delimiter = ',', skip_header = 1)[:, -1]
+dope_data = np.genfromtxt(dope_file, delimiter = ',', skip_header = 1)[:, -1]
+
+fig, ax = plt.subplots(1, 1, figsize = (8, 7))
+ax.plot(dope_data, '.', color = '#2F349A', markerfacecolor = 'w', markersize = 14, markeredgewidth = 2)
+ax.axhline(y = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+
+ax.set_ylabel(r'$E-E_f$ [eV]', fontsize = 20)
+ax.set_xticks([])
+ax.set_ylim((-0.3, 0.6))
+ax.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+
+fig.tight_layout()
+
+axin = inset_axes(ax, width = '100%', height = '100%', bbox_to_anchor=(0.05, .6, .45, .35), bbox_transform = ax.transAxes, loc = 3)
+axin.plot(dope_data*1000, '.', color = '#2F349A', markerfacecolor = 'w', markersize = 14, markeredgewidth = 2)
+axin.axhline(y = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+
+axin.set_ylabel(r'$E-E_f$ [meV]', fontsize = 20)
+axin.set_xticks([])
+axin.set_ylim((-50, 20))
+axin.set_xlim((80, 100))
+axin.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+axin.yaxis.set_label_position('right')
+axin.yaxis.tick_right()
+
+ax.add_patch(Rectangle((80, -0.05), 20, 0.07, edgecolor = 'k', fill = False, lw = 2))
+ax.add_patch(Rectangle((2.8, 0.248), 92.1, 0.318, edgecolor = 'k', fill = False, lw = 2))
+
+l1 = [(80, -0.05), (2.8, 0.248)]
+l2 = [(80, 0.02), (2.8, 0.566)]
+l3 = [(100, -0.05), (94.9, 0.25)]
+l4 = [(100, 0.02), (94.9, 0.566)]
+lc = LineCollection([l1, l2, l3, l4], color = 'k', linewidth = 2)
+ax.add_collection(lc)
+
+ax.annotate(r'One H in Ta$_{16}$P$_{16}$', (80, -0.25), fontsize = 20)
+
+plt.savefig('dope_weyl.png', dpi = 300)
+
+fig, ax = plt.subplots(1, 1, figsize = (8, 7))
+ax.plot(pris_data*1000, '.', color = '#762130', markerfacecolor = 'w', markersize = 14, markeredgewidth = 2)
+ax.axhline(y = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+
+ax.set_ylabel(r'$E-E_f$ [meV]', fontsize = 20)
+ax.set_ylim((-50, 20))
+ax.set_xticks([])
+ax.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+
+fig.tight_layout()
+
+ax.annotate(r'Ta$_{2}$P$_{2}$', (15, -46), fontsize = 20)
+
+plt.savefig('pris_weyl.png', dpi = 300)
