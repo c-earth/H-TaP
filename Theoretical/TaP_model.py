6224c06114da54c577b8f174e3c80aeb0738ce73
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-08-27 10:55:41 -0400

    theory figure

--- Theoretical/TaP_model.py
+++ Theoretical/TaP_model.py
@@ -6,16 +6,18 @@ dE = np.linspace(-50, 50, 1001)
 pi = math.pi
 
 ec = 8E15/(3*pi**2)*(2*((dE-24)**3+24**3)/1.9+((dE+40)**3-40**3)/1.1)
-print(8E15/(3*pi**2)*(2*((24-24)**3+24**3)/1.9+((24+40)**3-40**3)/1.1))
-print(8E15/(3*pi**2)*(2*((-40-24)**3+24**3)/1.9+((-40+40)**3-40**3)/1.1))
 
-fig, ax = plt.subplots(1, 1, figsize = (14, 7), dpi=300)
-ax.plot(dE, ec, 'r-')
-ax.axvline(x = 0, linewidth = 1, color = 'k', linestyle = (0, (8, 10)))
-ax.axhline(y = 0, linewidth = 1, color = 'k', linestyle = (0, (8, 10)))
-ax.set_xlabel(r'$E-E_f$ [meV]', fontsize = 24)
-ax.set_ylabel(r'Doped Electron [cm$^{-3}$]', fontsize = 24)
-ax.tick_params(axis = 'both', which = 'both', width = 2, length = 10, direction = 'in', labelsize = 24)
-ax.yaxis.get_offset_text().set_size(24)
-# plt.show()
-plt.savefig('TaP_model.png')
+fig, ax = plt.subplots(1, 1, figsize = (8, 7))
+ax.plot(dE, ec, '-', color = '#E33119', linewidth = 3)
+ax.axvline(x = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+ax.axhline(y = 0, linewidth = 2, color = 'k', linestyle = (0, (8, 10)))
+
+ax.set_xlabel(r'$E-E_f$ [meV]', fontsize = 20)
+ax.set_ylabel(r'Doped Electron [cm$^{-3}$]', fontsize = 20)
+ax.tick_params(axis = 'both', which = 'both', top = False, right = False, width = 1.5, length = 5, direction = 'in', labelsize = 20)
+ax.ticklabel_format(axis = 'y', style = 'sci', scilimits = (0, 0), useMathText = True)
+ax.yaxis.get_offset_text().set_size(20)
+
+fig.tight_layout()
+
+plt.savefig('TaP_model.png', dpi = 300)
